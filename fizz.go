a40b47c298ab02397d2705e95f5b6dd7e2f743d3
starseed777 <waress66@live.com>
2020-09-14 19:30:35 -0400

    code

---
+++ fizz.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	count := 100
+
+	for i := 0; i <= count; i++ {
+		if i%3 == 0 && i%5 == 0 {
+			fmt.Println("fizzbuzz")
+		} else if i%3 == 0 {
+			fmt.Println("fizz")
+		} else if i%5 == 0 {
+			fmt.Println("buzz")
+		} else {
+			fmt.Println(i)
+		}
+
+	}
+}
